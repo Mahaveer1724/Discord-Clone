c9cf99bb951ba2caf5a3699adea40f96eeb571a4
milan <panchalmilan1731@gmail.com>
2026-05-10 19:05:21 +0530

    frontend deleted (lost hope - 1)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login_page from './Final_Designs/Login_page'
-import './style.css'
-import './Final_Designs/Login_page.css'
-import Regstration_page from './Final_Designs/Registration_page/Registration_page'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Regstration_page />
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
-
