1568859f7d89b1333999353ed76c069d62c2d094
milan <panchalmilan1731@gmail.com>
2026-05-22 23:33:30 +0530

    Friend list added

--- frontend/src/components/ui/Friends.jsx
+++ frontend/src/components/ui/Friends.jsx
@@ -3,7 +3,66 @@ import React from "react";
 function Friends () {
     return(
         <div className="min-h-screen w-[20rem] bg-blue-300 absolute">
-            
+            <div className="h-[15rem] w-[20rem] bg-gray-500">
+
+            </div>
+            <div className="w-[20rem] h-[36.54rem] bg-[#000000] grid-rows text-[#888991]">
+                <div className="bg-[#000000] w-[19.2rem] h-[4rem] rounded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F1Logo.png" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Jailer
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rounded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F2Logo.png" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Milan
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rounded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F3Logo.png" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Jailer 2
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rouded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F4Logo.png" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Mitocondria
+                    </h1>
+                </div>
+                
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rouded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F6Logo.png" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Agent
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rouded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F7Logo.jpg" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Krishna Negi
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rouded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F8Logo.jpg" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Candy
+                    </h1>
+                </div>
+
+                <div className="w-[19.2rem] h-[4rem] bg-[#000000] rouded-lg -translate-x-[-0.4rem] hover:bg-[#121213] hover:text-white hover:cursor-pointer">
+                    <img src="./public/imgs/F9Logo.jpg" className="h-[3rem] w-[3rem] bg-center bg-no-repeat rounded-[1.5rem] mt-[0.5rem] absolute" />
+                    <h1 className="-translate-x-[-4rem] text-2xl mt-[1.1rem] absolute">
+                        Veer
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
